9bd3b9b8451e80e2230d4fe25edaa6118f6fb5e9
ghdic <ghdic77@gmail.com>
2021-04-29 23:08:33 +0900

    로그인 구현

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -1,2 +1,44 @@
-package dao;public class UserDAO {
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+    private Connection conn;
+    private PreparedStatement pstmt;
+    private ResultSet rs;
+
+    public UserDAO() {
+        try {
+            String dbURL = "jdbc:mysql://localhost:3306/simpleboard";
+            String dbID = "root";
+            String dbPW = "root";
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(dbURL, dbID, dbPW);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int login(String userID, String userPW) {
+        String SQL = "select userPW from user where userID = ?";
+        try {
+            pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1, userID);
+            rs = pstmt.executeQuery();
+            if(rs.next()) {
+                if(rs.getString(1).equals(userPW)) {
+                    return 1; // 로그인 성공
+                } else {
+                    return 0; // 비밀번호 불일치
+                }
+            }
+            return -1; // 아이디 없음
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -2; // 데이터베이스 오류
+    }
 }
--- web/logoutAction.jsp
+++ web/logoutAction.jsp
@@ -6,11 +6,10 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
-</html>
+<%
+	session.invalidate();
+%>
+
+<script>
+	location.href = 'index.jsp';
+</script>
